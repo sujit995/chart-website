3137f7ca442615cc3c72ed126e12bb066c9e591e
sujit995 <sujitmishra402@gmail.com>
2021-11-15 09:00:36 +0530

    adding details

--- src/App.js
+++ src/App.js
@@ -5,6 +5,11 @@ import GraphChart from './components/GraphChart';
 function App() {
   return (
     <div className="App">
+      <div>
+        <h4>name - sujit mishra</h4>
+        <p>email- sujitmishra115@gmail.com</p>
+        <p>contact - 7008493948</p>
+      </div>
       <GraphChart />
     </div>
   );
