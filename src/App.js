b424d53c9b0a690fe65effec7dc8629c964f5c26
sujit995 <sujitmishra402@gmail.com>
2021-11-15 09:21:51 +0530

    adding personal details

--- src/App.js
+++ src/App.js
@@ -5,12 +5,13 @@ import GraphChart from './components/GraphChart';
 function App() {
   return (
     <div className="App">
-      <div>
+      
+      <GraphChart />
+      <div className="details">
         <h4>name - sujit mishra</h4>
         <p>email- sujitmishra115@gmail.com</p>
         <p>contact - 7008493948</p>
       </div>
-      <GraphChart />
     </div>
   );
 }
